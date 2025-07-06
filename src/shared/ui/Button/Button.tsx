4f3daa49ea59a7975547b7657cc03d42c8159461
Gusarov Anatoly <gusarov.tolia@mail.ru>
2025-07-06 16:40:44 +0500

    feat: add Button

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,40 @@
+import type { ButtonHTMLAttributes, FC } from 'react';
+import cn from 'classnames';
+
+import styles from './Button.module.css';
+
+type Variant = 'primary' | 'secondary' | 'download' | 'upload' | 'clear';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+	children: React.ReactNode;
+
+	variant?: Variant;
+	fullWidth?: boolean;
+};
+
+export const Button: FC<ButtonProps> = ({
+	variant = 'primary',
+	children,
+	fullWidth = false,
+	className = '',
+	disabled = false,
+	...rest
+}) => {
+	return (
+		<button
+			className={cn(
+				styles.button,
+				styles[variant],
+				{
+					[styles.fullWidth]: fullWidth,
+					[styles.disabled]: disabled,
+				},
+				className
+			)}
+			disabled={disabled}
+			{...rest}
+		>
+			{children}
+		</button>
+	);
+};
