0f61e2097f487c5e8691b11a99cd8b4276554114
Gusarov Anatoly <gusarov.tolia@mail.ru>
2025-07-20 21:02:28 +0500

    fix: correct clear icon

--- src/shared/assets/icons/iconsName/Clear/Clear.tsx
+++ src/shared/assets/icons/iconsName/Clear/Clear.tsx
@@ -6,11 +6,11 @@ export const Clear: FC<IconBaseProps> = ({ size, ...rest }) => {
 	return (
 		<SvgBase size={size} {...rest}>
 			<path
-				stroke="#fff"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				d="M7.167 25.333 16.5 16m0 0 9.333-9.333M16.5 16 7.167 6.667M16.5 16l9.333 9.333"
+				d="M1.16675 20.3334L10.5001 11.0001M10.5001 11.0001L19.8334 1.66675M10.5001 11.0001L1.16675 1.66675M10.5001 11.0001L19.8334 20.3334"
+				stroke="white"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 		</SvgBase>
 	);
