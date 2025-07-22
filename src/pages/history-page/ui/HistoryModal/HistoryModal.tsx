22d1d1c45cab5967bb9e53ae47ca006c0270c183
Gusarov Anatoly <gusarov.tolia@mail.ru>
2025-07-22 14:28:24 +0500

    feat: add historyModal

---
+++ src/pages/history-page/ui/HistoryModal/HistoryModal.tsx
@@ -0,0 +1,42 @@
+import type { AnalysisHighlight } from '@/entities/highlights/model/types/highlight.type';
+import { HighlightCard } from '@/entities/highlights/ui/HighlightCard';
+import { convertHighlightsToArray } from '@/features/analyse-file/lib/convertHighlightsToArray';
+import { useHistoryStore } from '@/features/history/model/store';
+import { Modal } from '@/shared/ui/Modal/Modal';
+import type { FC } from 'react';
+import { useShallow } from 'zustand/shallow';
+import styles from './HistoryModal.module.css';
+
+export const HistoryModal: FC = () => {
+	const { isOpenModal, selectedItem, hideModal } = useHistoryStore(
+		useShallow((state) => ({
+			isOpenModal: state.isOpenModal,
+			selectedItem: state.selectedItem,
+			hideModal: state.hideModal,
+		}))
+	);
+
+	if (!selectedItem?.highlights) {
+		return null;
+	}
+
+	const hightlights: AnalysisHighlight[] = convertHighlightsToArray(
+		selectedItem.highlights
+	);
+
+	return (
+		<Modal isOpen={isOpenModal} onClose={hideModal}>
+			<div className={styles.root}>
+				<div className={styles.highlights}>
+					{hightlights.map((highlight) => (
+						<HighlightCard
+							key={highlight.title}
+							highlight={highlight}
+							className={styles.hightlightCard}
+						/>
+					))}
+				</div>
+			</div>
+		</Modal>
+	);
+};
