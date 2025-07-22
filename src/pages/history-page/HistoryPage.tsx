b8f266c6b13529c6ec33a6699dd2b3af7055d33b
Gusarov Anatoly <gusarov.tolia@mail.ru>
2025-07-22 14:30:12 +0500

    refactor: add components for correct work historyPage

--- src/pages/history-page/HistoryPage.tsx
+++ src/pages/history-page/HistoryPage.tsx
@@ -1,5 +1,20 @@
+import styles from './HistoryPage.module.css';
+import { ClearHistoryButton } from './ui/ClearHistoryButton';
+import { GenerateMoreButton } from './ui/GenerateMoreButton';
+import { HistoryList } from './ui/HistoryList';
+import { HistoryModal } from './ui/HistoryModal';
+
 const HistoryPage = () => {
-	return <div>history-page</div>;
+	return (
+		<div className={styles.container}>
+			<HistoryList />
+			<div className={styles.actions}>
+				<GenerateMoreButton />
+				<ClearHistoryButton />
+			</div>
+			<HistoryModal />
+		</div>
+	);
 };
 
 export default HistoryPage;
