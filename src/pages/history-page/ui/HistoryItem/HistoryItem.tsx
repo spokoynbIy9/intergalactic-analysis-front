397c3a39aabdd8aec6c9c9bd118456dcb8c45752
Gusarov Anatoly <gusarov.tolia@mail.ru>
2025-07-22 14:24:57 +0500

    feat: add historyItem

---
+++ src/pages/history-page/ui/HistoryItem/HistoryItem.tsx
@@ -0,0 +1,70 @@
+import type { HistoryItemType } from '@/features/history/model/types/historyItem.type';
+import type { FC } from 'react';
+import styles from './HistoryItem.module.css';
+import cn from 'classnames';
+import { Button } from '@/shared/ui/Button';
+import { Typography } from '@/shared/ui/Typography';
+import { formatDate } from '@/shared/helpers';
+import { File, Trash } from '@/shared/assets/icons/iconsName';
+import { FileStatus } from '../FileStatus/FileStatus';
+
+type HistoryItemProps = {
+	item: HistoryItemType;
+	onClick: (item: HistoryItemType) => void;
+	onDelete: (id: string) => void;
+};
+
+export const HistoryItem: FC<HistoryItemProps> = ({
+	item,
+	onClick,
+	onDelete,
+}) => {
+	const { timestamp, id, fileName, highlights } = item;
+
+	const date = formatDate(timestamp);
+
+	const hasHighlights = Boolean(highlights);
+
+	const handleDeleteButtonClick = () => {
+		onDelete(id);
+	};
+
+	const handleItemClick = () => {
+		if (!hasHighlights) {
+			return;
+		}
+
+		onClick(item);
+	};
+
+	return (
+		<div className={styles.root}>
+			<Button
+				type="button"
+				variant="secondary"
+				className={cn(styles.item, {
+					[styles.disabled]: !hasHighlights,
+				})}
+				aria-label={`Открыть хайлайты для ${fileName}`}
+				onClick={handleItemClick}
+			>
+				<div className={styles.fileName}>
+					<File size={40} className={styles.icon} />
+					<Typography maxRowsNumber={1}>{fileName}</Typography>
+				</div>
+				<Typography>{date}</Typography>
+				<FileStatus type="success" isActive={hasHighlights} />
+				<FileStatus type="error" isActive={!hasHighlights} />
+			</Button>
+			<Button
+				type="button"
+				variant="clear"
+				className={styles.deleteButton}
+				aria-label={`Удалить файл ${fileName}`}
+				onClick={handleDeleteButtonClick}
+			>
+				<Trash size={33} />
+			</Button>
+		</div>
+	);
+};
