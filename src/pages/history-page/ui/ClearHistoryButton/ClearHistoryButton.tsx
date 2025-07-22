54e3dbfd9c0ad06dbafece9d20c2756a490e27b8
Gusarov Anatoly <gusarov.tolia@mail.ru>
2025-07-22 14:28:07 +0500

    feat: add clearHistoryButton

---
+++ src/pages/history-page/ui/ClearHistoryButton/ClearHistoryButton.tsx
@@ -0,0 +1,22 @@
+import { useHistoryStore } from '@/features/history/model/store';
+import { Button } from '@/shared/ui/Button';
+import { clearHistory as clearHistoryStorage } from '@/features/history/lib/historyStorage';
+
+export const ClearHistoryButton = () => {
+	const { clearHistory, history } = useHistoryStore();
+
+	const handleClearHistory = () => {
+		clearHistory();
+		clearHistoryStorage();
+	};
+
+	if (history.length === 0) {
+		return null;
+	}
+
+	return (
+		<Button variant="clear" onClick={handleClearHistory}>
+			Очистить всё
+		</Button>
+	);
+};
