9ef3113b244fbc9e530ae4610dda1ea8aacdd6fa
Gusarov Anatoly <gusarov.tolia@mail.ru>
2025-07-25 21:36:09 +0500

    build: install vitest and other connected, set up vitest.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
 	plugins: [react()],
 	resolve: {
 		alias: [{ find: '@', replacement: '/src' }],
 	},
-});
+}));
