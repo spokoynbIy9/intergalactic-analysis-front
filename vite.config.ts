8963a7a4bb77b4032efa992817285327bd7b97d8
Gusarov Anatoly <gusarov.tolia@mail.ru>
2025-07-26 23:12:42 +0500

    chore: update configs for testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig(() => ({
 	plugins: [react()],
 	resolve: {
-		alias: [{ find: '@', replacement: '/src' }],
+		alias: [
+			{ find: '@', replacement: '/src' },
+			{
+				find: '@tests',
+				replacement: path.resolve(__dirname, 'tests'),
+			},
+		],
 	},
 }));
